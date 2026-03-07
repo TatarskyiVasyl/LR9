701935ce370f6f69c0e29d7136bb277ae39f80ec
TatarskyiVasyl <tatarskyi.vasyl.kn@dspu.edu.ua>
2026-03-07 20:54:51 +0200

    Add files via upload

---
+++ LR9.1.B.cpp
@@ -0,0 +1,175 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+enum Specialty { COMP_SCIENCE, INFORMATICS, MATH_ECON, PHYS_INFO, LABOR_EDU };
+
+string specialtyStr[] = {
+    "Комп'ютерні науки",
+    "Інформатика",
+    "Математика та економіка",
+    "Фізика та інформатика",
+    "Трудове навчання"
+};
+
+struct Student
+{
+    string    prizv;
+    int       kurs;
+    Specialty specialty;
+    int       phys;
+    int       math;
+    union
+    {
+        int programming;
+        int num_methods;
+        int pedagogy;
+    };
+};
+
+void   Create(Student* s, const int N);
+void   Print(Student* s, const int N);
+double Task1(Student* s, const int N);
+void   Task2(Student* s, const int N);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N;
+    cout << "Введіть кількість студентів N: ";
+    cin >> N;
+
+    Student* s = new Student[N];
+
+    Create(s, N);
+    Print(s, N);
+
+    double proc = Task1(s, N);
+    cout << "Процент студентів, які вчаться без трійок: "
+        << fixed << setprecision(2) << proc << "%" << endl;
+
+    cout << "\nПрізвища студентів, які отримали з фізики оцінку 5 або 4:" << endl;
+    Task2(s, N);
+
+    delete[] s;
+    return 0;
+}
+
+void Create(Student* s, const int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << "\nСтудент № " << i + 1 << ":" << endl;
+
+        cin.get();
+        cin.sync();
+
+        cout << "   прізвище    : "; getline(cin, s[i].prizv);
+        cout << "   курс        : "; cin >> s[i].kurs;
+
+        cout << "   спеціальність (0 - Комп'ютерні науки, 1 - Інформатика,\n"
+            "                   2 - Математика та економіка, 3 - Фізика та інформатика,\n"
+            "                   4 - Трудове навчання): ";
+        int sp; cin >> sp;
+        s[i].specialty = (Specialty)sp;
+
+        cout << "   оцінка з фізики     : "; cin >> s[i].phys;
+        cout << "   оцінка з математики : "; cin >> s[i].math;
+
+        switch (s[i].specialty)
+        {
+        case COMP_SCIENCE:
+            cout << "   оцінка з програмування    : ";
+            cin >> s[i].programming;
+            break;
+        case INFORMATICS:
+            cout << "   оцінка з чисельних методів: ";
+            cin >> s[i].num_methods;
+            break;
+        default:
+            cout << "   оцінка з педагогіки       : ";
+            cin >> s[i].pedagogy;
+            break;
+        }
+
+        cout << endl;
+    }
+}
+
+string thirdSubject(Specialty sp)
+{
+    switch (sp)
+    {
+    case COMP_SCIENCE: return "Програмування";
+    case INFORMATICS:  return "Числ. методи";
+    default:           return "Педагогіка";
+    }
+}
+
+void Print(Student* s, const int N)
+{
+    cout << "\n=================================================================="
+        "==========================================" << endl;
+    cout << "| №  | Прізвище             | Курс | Спеціальність              "
+        "| Фіз | Мат | Третя дисципліна     | Оц. |" << endl;
+    cout << "------------------------------------------------------------------"
+        "------------------------------------------" << endl;
+
+for (int i = 0; i < N; i++)
+{
+    int third;
+    switch (s[i].specialty)
+    {
+    case COMP_SCIENCE: third = s[i].programming; break;
+    case INFORMATICS:  third = s[i].num_methods;  break;
+    default:           third = s[i].pedagogy;     break;
+    }
+
+    cout << "| " << setw(2) << right << i + 1 << " ";
+    cout << "| " << setw(20) << left << s[i].prizv << " ";
+    cout << "| " << setw(4) << right << s[i].kurs << " ";
+    cout << "| " << setw(26) << left << specialtyStr[s[i].specialty] << " ";
+    cout << "| " << setw(3) << right << s[i].phys << " ";
+    cout << "| " << setw(3) << right << s[i].math << " ";
+    cout << "| " << setw(20) << left << thirdSubject(s[i].specialty) << " ";
+    cout << "| " << setw(3) << right << third << " |" << endl;
+}
+
+cout << "=================================================================="
+"==========================================" << endl;
+}
+
+double Task1(Student* s, const int N)
+{
+    int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int third;
+        switch (s[i].specialty)
+        {
+        case COMP_SCIENCE: third = s[i].programming; break;
+        case INFORMATICS:  third = s[i].num_methods;  break;
+        default:           third = s[i].pedagogy;     break;
+        }
+
+        if (s[i].phys >= 4 && s[i].math >= 4 && third >= 4)
+            k++;
+    }
+    return 100.0 * k / N;
+}
+
+void Task2(Student* s, const int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (s[i].phys == 5 || s[i].phys == 4)
+        {
+            cout << setw(3) << right << i + 1
+                << " " << s[i].prizv << endl;
+        }
+    }
+}
